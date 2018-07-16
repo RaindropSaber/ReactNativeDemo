63b897a0c1b6d2d3aa0404f48f73bd7d04614793
RaindropSaber <38107586+RaindropSaber@users.noreply.github.com>
2018-07-16 17:12:17 +0800

    Audio地址修改

--- pages/Audio/Audio.js
+++ pages/Audio/Audio.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import {AppRegistry,Platform, StyleSheet, Text, View,ScrollView,Button} from 'react-native';
 import Sound from 'react-native-sound';
+import RNFS from 'react-native-fs'
 Sound.setCategory('Playback');
-
+const url3= `${RNFS.DocumentDirectoryPath}/test.mp3`
 const url2="file:///Users/ugen/Music/%E7%BD%91%E6%98%93%E4%BA%91%E9%9F%B3%E4%B9%90/test.mp3"
 const url="https://music.163.com/song/media/outer/url?id=27937466.mp3"
 export default class Audio extends React.Component {
@@ -15,8 +16,50 @@ export default class Audio extends React.Component {
             s:{}
         }
     }
-    soundLoad(){
-        this.state.s = new Sound(url2,null, (e) => {
+    downloadFile() {
+        return new Promise((r,j)=>{
+            const downloadDest = `${RNFS.DocumentDirectoryPath}/test.mp3`;
+            const formUrl="http://192.168.2.234:8000/test.mp3"
+            const options = {
+                fromUrl: formUrl,
+                toFile: downloadDest,
+                background: true,
+                begin: (res) => {
+                    console.warn('begin', res);
+                    console.warn('contentLength:', res.contentLength / 1024 / 1024, 'M');
+                },
+                progress: (res) => {
+        
+                    let pro = res.bytesWritten / res.contentLength;
+        
+                    this.setState({
+                        progressNum: pro,
+                    });
+                }
+            };
+            try {
+                const ret = RNFS.downloadFile(options);
+                ret.promise.then(res => {
+                    console.warn('success', res);
+                    this.setState({
+                        downloadDest: 'file://' + downloadDest,
+                    });
+                    r(downloadDest)
+                    console.warn('file://' + downloadDest)
+                }).catch(err => {
+                    console.warn('err', err);
+                    j()
+                });
+            }
+            catch (e) {
+                console.warn("ssss",error);
+                j(e)
+            }
+        })        
+    }
+    async soundLoad(){
+        downloadDest =await this.downloadFile()
+        this.state.s = new Sound(downloadDest,null, (e) => {
             if (e) {
                  console.warn('音乐加载失败');
                 return;
